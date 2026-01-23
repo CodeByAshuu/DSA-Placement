a4acf3286426453b08c78f481ffb55b518e8d38d
Sagar <sagarsahu5976@gmail.com>
2026-01-23 16:01:52 +0530

    Triple pair sum

--- Arrays/TriplePairSum.cpp
+++ Arrays/TriplePairSum.cpp
@@ -19,7 +19,8 @@ int main(){
     for(int i =0; i<n-1; i++){
         for(int j =i+1; j<n; j++){
             if(arr[i]+arr[j] == value){
-                cout << arr[i] << " + " << arr[j] << " = " << value <<endl;
+                // cout << arr[i] << " + " << arr[j] << " = " << value <<endl;
+                cout << "[" << i << ", " << j << "]" <<endl;
             } 
         }
     }
